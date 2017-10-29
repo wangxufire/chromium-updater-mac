48823bc8256220a54fddc127f34c3ea1bd53341f
星野ハジメ <wangxufire+github@gmail.com>
2017-10-29 18:48:33 +0800

    format

--- chromium_update.py
+++ chromium_update.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 Chromium Updater
 Download and install the newest Chromium build
@@ -8,7 +7,7 @@ from http://chromium.org on Mac OS X.
 """
 
 from __future__ import absolute_import, division, print_function, \
-    with_statement
+    with_statement, unicode_literals
 
 import logging
 import os.path
@@ -16,7 +15,6 @@ import requests
 import sys
 import traceback
 from contextlib import closing
-
 
 LAST_REVISION_URL = 'https://commondatastorage.googleapis.com/\
 chromium-browser-snapshots/Mac/LAST_CHANGE'
@@ -28,8 +26,9 @@ DOWNLOAD_PATH = os.path.expanduser('~/.chromium_updater/')
 
 
 def latest_revision():
-    logging.info('Looking for latest revision from %s...' %
-                 (LAST_REVISION_URL))
+    logging.info(
+      'Looking for latest revision from %s...' % (LAST_REVISION_URL)
+    )
     return requests.get(LAST_REVISION_URL).text
 
 
@@ -37,7 +36,8 @@ def install():
     logging.debug('Extracting files ...')
     if os.system('unzip -q chrome-mac.zip') != 0:
         logging.error(
-            'Failed to update maybe the zip file not download success.')
+          'Failed to update maybe the zip file not download success.'
+        )
         sys.exit(1)
     logging.debug('Removing old files.')
     os.system('rm -rf /Applications/Chromium.app')
@@ -50,9 +50,10 @@ def install():
 
 def update():
     logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)-8s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
+      level=logging.DEBUG,
+      format='%(asctime)s %(levelname)-8s %(message)s',
+      datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
     revision = latest_revision()
     logging.info('Latest revision is %s.' % (revision))
@@ -75,12 +76,13 @@ def update():
             chunk_size = 1024
             content_size = int(response.headers['content-length'])
             progress = ProgressBar(
-                'progressing',
-                total=content_size,
-                unit='KB',
-                chunk_size=chunk_size,
-                run_status=to_str('正在下载'),
-                fin_status=to_str('下载完成'))
+              'progressing',
+              total=content_size,
+              unit='KB',
+              chunk_size=chunk_size,
+              run_status='正在下载',
+              fin_status='下载完成'
+            )
             with open('./chrome-mac.zip', "wb") as file:
                 for data in response.iter_content(chunk_size=chunk_size):
                     file.write(data)
@@ -93,7 +95,8 @@ def update():
     logging.info('Upgraded to revision %s' % (revision))
     os.system('killall Chromium')
     os.system(
-        'open -a /Applications/Chromium.app --args --vmodule=google_api_keys=1'
+      'open -a /Applications/Chromium.app --args \
+      --vmodule=google_api_keys=1 $CHROMIUM_OPTIONS'
     )
     sys.exit(0)
 
@@ -105,30 +108,34 @@ Link：http://blog.csdn.net/supercooly/article/details/51046561
 
 
 class ProgressBar(object):
-    def __init__(self,
-                 title,
-                 count=0.0,
-                 run_status=None,
-                 fin_status=None,
-                 total=100.0,
-                 unit='',
-                 sep='/',
-                 chunk_size=1.0):
+
+    def __init__(
+      self,
+      title,
+      count=0.0,
+      run_status=None,
+      fin_status=None,
+      total=100.0,
+      unit='',
+      sep='/',
+      chunk_size=1.0
+    ):
         super(ProgressBar, self).__init__()
         self.info = '[%s] %s %.2f %s %s %.2f %s'
         self.title = title
         self.total = total
         self.count = count
         self.chunk_size = chunk_size
-        self.status = run_status or b''
-        self.fin_status = fin_status or b' ' * len(self.status)
+        self.status = run_status or ''
+        self.fin_status = fin_status or ' ' * len(self.status)
         self.unit = unit
         self.seq = sep
 
     def __get_info(self):
-        _info = self.info % (self.title, self.status,
-                             self.count / self.chunk_size, self.unit, self.seq,
-                             self.total / self.chunk_size, self.unit)
+        _info = self.info % (
+          self.title, self.status, self.count / self.chunk_size, self.unit,
+          self.seq, self.total / self.chunk_size, self.unit
+        )
         return _info
 
     def refresh(self, count=1, status=None):
@@ -139,13 +146,6 @@ class ProgressBar(object):
             end_str = '\n'
             self.status = status or self.fin_status
         print(self.__get_info(), end=end_str)
-
-
-def to_str(s):
-    if bytes != str:
-        if type(s) == bytes:
-            return s.decode('utf-8')
-    return s
 
 
 if __name__ == '__main__':
