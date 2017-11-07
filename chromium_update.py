f952ba3bc43dc9cdcc81a339e28d4da3a6f6d602
星野ハジメ <wangxufire+github@gmail.com>
2017-11-07 13:46:46 +0800

    fix

--- chromium_update.py
+++ chromium_update.py
@@ -16,6 +16,17 @@ import sys
 import traceback
 from contextlib import closing
 
+try:
+    reload  # Python 2.7
+except NameError:
+    try:
+        from importlib import reload  # Python 3.4+
+    except ImportError:
+        from imp import reload  # Python 3.0 - 3.3
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 LAST_REVISION_URL = 'https://commondatastorage.googleapis.com/\
 chromium-browser-snapshots/Mac/LAST_CHANGE'
 
