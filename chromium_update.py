d13c71d378c44a93897f6f627694d10a7cfe304b
星野ハジメ <wangxufire+github@gmail.com>
2018-07-19 12:08:47 +0800

    update

--- chromium_update.py
+++ chromium_update.py
@@ -10,9 +10,10 @@ from __future__ import absolute_import, division, print_function, \
     with_statement, unicode_literals
 
 import logging
-import os.path
+import os
 import requests
 import sys
+import subprocess
 import traceback
 from contextlib import closing
 
@@ -25,7 +26,11 @@ except NameError:
         from imp import reload  # Python 3.0 - 3.3
 
 reload(sys)
-sys.setdefaultencoding("utf-8")
+
+try:
+    sys.setdefaultencoding("utf-8")
+except Exception:
+    pass
 
 LAST_REVISION_URL = 'https://commondatastorage.googleapis.com/\
 chromium-browser-snapshots/Mac/LAST_CHANGE'
@@ -50,11 +55,11 @@ def install():
           'Failed to update maybe the zip file not download success.'
         )
         sys.exit(1)
-    logging.debug('Removing old files.')
+    logging.info('Removing old app.')
     os.system('rm -rf /Applications/Chromium.app')
-    logging.debug('Installing files.')
+    logging.info('Installing new app.')
     os.system('cp -r chrome-mac/ /Applications/')
-    logging.debug('Removing old files ...')
+    logging.info('Removing download files ...')
     os.system('rm -rf chrome-mac*')
     logging.info('All done!')
 
@@ -80,7 +85,7 @@ def update():
     os.mkdir(local_download_path)
     os.chdir(local_download_path)
     download_url = DOWNLOAD_URL.format(revision)
-    logging.debug('Starting download chromium from: %s' % (download_url))
+    logging.info('Starting download chromium from: %s' % (download_url))
 
     try:
         with closing(requests.get(download_url, stream=True)) as response:
@@ -98,16 +103,25 @@ def update():
                 for data in response.iter_content(chunk_size=chunk_size):
                     file.write(data)
                     progress.refresh(count=len(data))
-    except:
+    except Exception:
         traceback.print_exc()
         sys.exit(1)
 
+    logging.info('Upgrade to revision %s' % (revision))
     install()
-    logging.info('Upgraded to revision %s' % (revision))
-    os.system('killall Chromium')
+    child = subprocess.Popen(
+      ['pgrep', '-f', 'Chromium'], stdout=subprocess.PIPE, shell=False
+    )
+    resp = child.communicate()[0]
+    if resp.strip():
+        pid = int(resp.split()[0])
+        logging.info('Kill old process %s' % (pid))
+        os.system('\kill -QUIT %s' % (pid))
+
+    logging.info('Start a new instance')
     os.system(
       'open -a /Applications/Chromium.app --args \
-      --vmodule=google_api_keys=1 $CHROMIUM_OPTIONS'
+      --vmodule=google_api_keys=1 %s' % (os.environ.get('CHROMIUM_OPTIONS'))
     )
     sys.exit(0)
 
